82e0fc15ed6f8c395c6bd8d6ffc59c0261eadb19
Val_F4 <vallkemgamer@gmail.com>
2023-08-28 16:32:33 -0300

    Commit | Configurando o menu para telas grandes

--- js/menu.js
+++ js/menu.js
@@ -6,4 +6,13 @@ function click_menu() {
     else {
         options.style.display = 'block'
     }
+    
+}
+
+function new_windows() {
+    if (window.innerWidth >= 769)
+        options.style.display = 'block'
+    else
+        options.style.display = 'none'
+
 }
